a982f05e743b272475ff2bca06d46d9a3fb80d50
na4lmeida <nazyalmeida@gmail.com>
2026-05-21 19:24:13 -0300

    ajuste no cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,12 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://fala-gtech.vercel.app"
+  ]
+}));
 
 const PORT = process.env.PORT || 3001;
 const SECRET_KEY =
